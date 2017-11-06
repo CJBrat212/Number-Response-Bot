2329a068a43850affb52e3480e63fae5d931be7e
Collin <Cjbrat21232@gmail.com>
2017-11-06 14:57:10 -0800

    Second Initial

--- number-response-bot.py
+++ number-response-bot.py
@@ -1,3 +1,21 @@
 import logging
 
 logging.basicConfig(level=logging.INFO)
+import asyncio
+import discord
+from discord.ext import commands
+import random
+
+description = "A bot for guessing numbers."
+
+client = discord.Client()
+client.login('token')
+client.token = "###"
+
+bot = commands.Bot(command_prefix='g!', description=description)
+
+@client.event
+async def on_ready():
+    print('logged in ')
+
+bot.run('token')
