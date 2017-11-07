66481f8c42f2157a39b4540361da4b8af75f002d
Collin <Cjbrat21232@gmail.com>
2017-11-06 17:29:45 -0800

    Basic Loop
    A very basic loop of random number generation, need to figure out how to get it to read the other bots message formatting

--- number-response-bot.py
+++ number-response-bot.py
@@ -7,8 +7,6 @@ client = discord.Client()
 
 description = "A bot for guessing numbers."
 
-bot = commands.Bot(command_prefix='g!', description=description)
-
 @client.event
 async def on_ready():
     print('Logged in as')
@@ -16,8 +14,30 @@ async def on_ready():
     print(client.user.id)
     print('------')
 
-@bot.command()
-async def ping():
-    await bot.say('Pong! Hello!')
+@client.event
+async def on_message(message):
+    if message.content.startswith('!test'):
+        await client.send_message(message.channel, 'what')
+    if message.content.startswith('start'):
+        await client.send_message(message.channel, 'Beginning program. ' )
+        await client.send_message(message.channel, 'g!start' )
+        y = 0
+        z = 100000
+        state = True
+        while(state == True):
+            hl = True
+            number = random.randint(y, z)
+            await client.send_message(message.channel, 'g!guess ' + str(number))
+            async def on_message(message):
+                if message.content.contains('**Higher!**'):
+                    y += 1000
+                    hl = True
+                if message.content.startswith('**Lower!**'):
+                    z -= 1000
+                    hl = False
+                if message.content.startswith('!Stop'):
+                    state = False
+            await client.send_message(message.channel, str(y) + ' and ' + str(z) + ' and ' + str(hl))
+        await client.send_message(message.channel, 'g!stop' )
 
-bot.run('token_here')
+client.run('user', 'password')
